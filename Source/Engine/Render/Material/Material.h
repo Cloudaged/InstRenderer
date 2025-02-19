d01d8ca9e7299f1f8c62cc16abf8f0c75646a3ae
AGXYE <133179396+Cloudaged@users.noreply.github.com>
2025-02-19 19:28:46 +0800

    finish base

--- Source/Engine/Render/Material/Material.h
+++ Source/Engine/Render/Material/Material.h
@@ -7,9 +7,9 @@ using Handle = uint32_t;
 
 struct alignas(16) Material
 {
-    Handle baseColor;
-    Handle normal;
-    Handle metallicRoughness;
+    Handle baseColor=0;
+    Handle normal=0;
+    Handle metallicRoughness=0;
     Handle pad0 = 0;
 };
 
